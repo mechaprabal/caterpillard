bd88da9ce5159d3d3c6e1edf13695d95afa82e57
Prabal Pratap Singh <mechaprabal@gmail.com>
2023-01-11 23:46:18 +0530

    version change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2023, Prabal Pratap Singh"
 author = "Prabal Pratap Singh"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.0.2"
 
 
 # -- General configuration ---------------------------------------------------
